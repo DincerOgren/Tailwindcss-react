56d431f23ee43aa891fa26d06f17a15891b16a21
DinçerÖğren <dincer.ogr@hotmail.com>
2025-04-20 23:57:52 +0300

    First tailwind project with react. Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import DarkModeToggle from './DarkModeToggle'
+
+function App() {
+
+  return (
+   <div className='bg-gray-100 p-4'>
+    <DarkModeToggle/>
+      <ui className='list-none md:flex md:gap-4'>
+        <li className='bg-blue-400 text-white p-2 m-2 rounded-none md:px-10'>Item 1</li>
+        <li className='bg-blue-400 text-white p-2 m-2 rounded-none md:px-10'>Item 2</li>
+        <li className='bg-blue-400 text-white p-2 m-2 rounded-none md:px-10'>Item 3</li>
+      </ui>
+   </div>
+  )
+}
+
+export default App
---
+++ src/Example.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='p-4 bg-white min-h-screen'>
+        <h1 className='text-2xl'>Dark mode text </h1>
+        <p>This is example for dark mode using tailwind css.</p>
+   </div>
+  )
+}
+
+export default App
